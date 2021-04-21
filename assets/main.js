80ffeb86c1a71979fe6e9da44ec91365639569e8
AjayNaruka <naruka.ajay1997@gmail.com>
2021-04-21 16:42:06 +0200

    JS funzionante

---
+++ assets/main.js
@@ -0,0 +1,55 @@
+$(function(){
+
+  var openMenu;
+  var activeIndex;
+  var isActive = false;
+
+ $(".main-list .hoverable").mouseenter(function(){
+   if(!isActive){
+    var clickedLi = $(this).find(".drop-menu");
+    clickedLi.addClass("active");
+    isActive=true;
+    activeIndex = $(this).index();
+    console.log("l'indice aperto e' : "+activeIndex)
+   }
+    
+ })
+
+ //CHIUSURA CON CLICK -> due casi
+ $(".main-list .hoverable").click(function(){
+   var clickedIndex = $(this).index();
+   console.log("hai cliccato su index: "+clickedIndex);   
+   console.log("hai cliccato su index: "+clickedIndex);   
+   if(isActive){
+     //CASO 1:  SE CLICCO LO STESSO
+     if($(this).index()===activeIndex){ 
+      var clickedLi = $(this).find(".drop-menu.active");
+      clickedLi.removeClass("active")
+      isActive=false;
+      activeIndex =null;
+      console.log("rimosso cliccando lo stesso link")
+     }
+     //CASO 2: ho cliccato un diverso link => chiudo il corrente e apro quello nuovo
+     else{ 
+      var alreadyOpen =$(".hoverable .drop-menu.active");
+      alreadyOpen.removeClass("active")
+      var clickedLi = $(this).find(".drop-menu");
+      clickedLi.addClass("active")
+      activeIndex=clickedIndex
+      console.log("rimosso aprendo un link diverso")
+     }
+   }
+ })
+
+
+ //HOVER MOUSE USCITA ( chiusura menu)
+ /* hoverable.mouseleave(function(){
+  //console.log(this)
+  var listItem = $(this).find(".drop-menu")
+  //console.log(listItem)
+  listItem.addClass("inactive")
+  isDown = false;
+}) */
+
+
+})

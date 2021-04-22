8cefd141b3f0c820b71ee73fe90bcd3e3c7952e5
AjayNaruka <naruka.ajay1997@gmail.com>
2021-04-22 12:36:03 +0200

    CORREZIONE JS

--- assets/main.js
+++ assets/main.js
@@ -4,23 +4,22 @@ $(function(){
   var isActive = false;
 
   //HOVER
- $(".main-list .hoverable").mouseenter(function(){
+ /* $(".main-list .hoverable").mouseenter(function(){
    if(!isActive){ // PER APRIRNE SOLO UNA ALLA VOLTA CON HOVER
-    var clickedLi = $(this).find(".drop-menu");
-    clickedLi.addClass("active");
+    var hoverLi = $(this).find(".drop-menu");
+    hoverLi.addClass("active");
     isActive=true;
     activeIndex = $(this).index();
     console.log("l'indice aperto e' : "+activeIndex)
-   }
-    
+   }  
  })
-
+ */
  //CHIUSURA -> due casi
- $(".main-list .hoverable").click(function(){
+ /* $(".main-list .hoverable").click(function(){
    var clickedIndex = $(this).index();
    console.log("hai cliccato su index: "+clickedIndex);   
    console.log("hai cliccato su index: "+clickedIndex);   
-   if(isActive){
+   if(isActive){ // ENTRO SOLO LE NE HO UN MENU ATTIVO
      //CASO 1:  SE CLICCO LO STESSO
      if($(this).index()===activeIndex){ 
       var clickedLi = $(this).find(".drop-menu.active");
@@ -39,8 +38,22 @@ $(function(){
       console.log("rimosso aprendo un link diverso")
      }
    }
- })
+ }) */
 
+//////////////////////////////
+// CORREZIONE
+//////////////////////////////
+$(".hoverable").mouseenter(function(){
+  //RIMUOVO DA TUTTI I MENU LA CLASSE ACTIVE
+  $(".drop-menu").removeClass("active")
+  //AGGIUNGO ACTIVE A THIS
+  $(this).find(".drop-menu").addClass("active")
+})
 
+$(".hoverable").click(function(){ // QUANDO CLICCO HO GIA FATTO UN HOVER DI PER SW
+  $(this).find(".drop-menu").toggleClass("active")
+})
 
 })
+
+
